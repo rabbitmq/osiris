46fc953a3270531905fa3a89c6dbd76a234fa7c0
Michael Davis <mcarsondavis@gmail.com>
2025-07-16 17:21:40 -0400

    Split out default osiris_log_reader impl

--- src/osiris_log_reader.erl
+++ src/osiris_log_reader.erl
@@ -1,5 +1,7 @@
 -module(osiris_log_reader).
 
+-export([open/1, pread/3, sendfile/4, close/1]).
+
 -type reader() :: term().
 
 -export_type([reader/0]).
@@ -19,3 +21,17 @@
     {ok, BytesWritten :: non_neg_integer()} | {error, term()}.
 
 -callback close(reader()) -> ok | {error, term()}.
+
+%% --- Default implementation
+
+open(SegmentFilename) ->
+    file:open(SegmentFilename, [raw, binary, read]).
+
+pread(Fd, Offset, Bytes) ->
+    file:pread(Fd, Offset, Bytes).
+
+sendfile(Fd, Socket, Offset, Bytes) ->
+    file:sendfile(Fd, Socket, Offset, Bytes, []).
+
+close(Fd) ->
+    file:close(Fd).
